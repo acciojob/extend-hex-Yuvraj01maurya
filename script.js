7f2076fdf844bd95565727459c09f50cd4e5f65d
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-08 21:24:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const extendHex = (shortHex) => {
   // write your code here
+  shortHex = shortHex.replace('#', '').toLowerCase();
+
+  // Separate the shortHex into individual color components
+  const red = shortHex[0];
+  const green = shortHex[1];
+  const blue = shortHex[2];
+
+  // Extend each color component to form the full hex code
+  const fullHex = `#${red}${red}${green}${green}${blue}${blue}`;
+
+  return fullHex;
 };
 
 // Do not change the code below.
